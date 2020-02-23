ab42871a5746142e692a8e4c59c39c241eff53b9
pepilica <57628520+pepilica@users.noreply.github.com>
2020-02-23 11:17:11 +0300

    Результат отбора

--- yl1.py
+++ yl1.py
@@ -235,5 +235,33 @@ def choice(planet):
 </html>'''.format(planet)
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return '''<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>Выбор планеты</title>
+      <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous">
+    </head>
+    <body>
+      <h1>Результаты отбора</h1>
+      <h5>претендента на участия в миссии {}</h5>
+        <div class="alert alert-success" role="alert">
+          Поздравляем! После {} этапа отбора ваш рейтинг 
+        </div>
+        <div class="alert alert-dark" role="alert">
+            составляет {}
+        </div>
+        <div class="alert alert-info" role="alert">
+            Желаем удачи!
+        </div>
+    </body>
+    </html>'''.format(nickname, level, rating)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
